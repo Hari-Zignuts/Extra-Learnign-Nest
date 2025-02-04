1c18075cfce0a7935a7c0fc4201d94a81fe0e0bb
Hari Malam <harim@zignuts.com>
2025-02-04 17:20:50 +0530

    implement cookies (get-set)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,12 +3,15 @@ import {
   Get,
   ParseFilePipeBuilder,
   Post,
+  Req,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
+import { Request, Response } from 'express';
 
 const storage = diskStorage({
   destination: './uploads/', // Ensure this folder exists
@@ -46,5 +49,17 @@ export class AppController {
   ) {
     console.log(file);
     return { message: 'File uploaded successfully', file: file.filename };
+  }
+
+  @Get('set-cookie')
+  setCookie(@Res({ passthrough: true }) res: Response) {
+    res.cookie('token', '123456789');
+    return { message: 'Cookie set successfully' };
+  }
+
+  @Get('get-cookie')
+  getCookie(@Req() req: Request) {
+    const token: unknown = req.cookies['token'];
+    return { message: 'Cookie retrieved successfully', token };
   }
 }
