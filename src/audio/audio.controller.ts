2765e78e745fdd632d8cfc68afc3e7abd8eddbe8
Hari Malam <harim@zignuts.com>
2025-02-05 12:29:15 +0530

    feat: session

--- src/audio/audio.controller.ts
+++ src/audio/audio.controller.ts
@@ -1,5 +1,5 @@
 import { InjectQueue } from '@nestjs/bull';
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Session } from '@nestjs/common';
 import { Queue } from 'bull';
 
 @Controller('audio')
@@ -9,11 +9,18 @@ export class AudioController {
     private readonly audioQueue: Queue,
   ) {}
 
-  @Post('transcode')
-  async transcode() {
+  @Get('transcode')
+  async transcode(@Session() session: Record<string, any>) {
+    session.user = { id: 1, username: 'Jane' };
     await this.audioQueue.add('transcode', {
       file: 'audio.mp3',
     });
     return { message: 'Job added to queue' };
+  }
+
+  @Get('transcode-with-session')
+  transcodeWithSession(@Session() session: Record<string, any>) {
+    const user: unknown = session.user;
+    return { user };
   }
 }
